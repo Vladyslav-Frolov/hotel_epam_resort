2f9e2152e9f42a707e668edc4054c82ad7adeb29
Vladyslav-Frolov <tertey7@gmail.com>
2020-09-28 14:28:58 +0300

    commit 1

--- src/main/java/hotel/MainServlet.java
+++ src/main/java/hotel/MainServlet.java
@@ -11,15 +11,24 @@ public class MainServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
+        log("Method init :) ");
     }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Method service enter\n");
         super.service(req, resp);
+        resp.getWriter().write("Method service exit\n");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Method doGet\n");
     }
 
     @Override
     public void destroy() {
-        super.destroy();
+        log("Method destroy :) ");
     }
+
 }
